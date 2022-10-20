cf97a11b8f1b12d822401d68a833373ba45c533b
gautam77 <103405748+guatam77@users.noreply.github.com>
2022-10-20 17:08:06 +0530

    new add code

---
+++ a.py
@@ -0,0 +1,11 @@
+payment_info=[[14, 80], [20, 50], [20, 50]]
+start=1
+end=22
+for i in range(start,end):
+   for j in payment_info:
+       r = j[0]
+       v = j[1]
+      
+       if i == r :
+           
+        
